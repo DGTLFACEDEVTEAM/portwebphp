2b0994bbcd171ce0303d2cb42f94e374be97a410
Cenk Arslan <c.arslan3509@gmail.com>
2024-01-09 13:42:20 +0300

    contact section kvkk update

--- resources/views/frontend/tr/contact/contacts.blade.php
+++ resources/views/frontend/tr/contact/contacts.blade.php
@@ -94,25 +94,38 @@
                                 </div>
                                 <div class="contact-form-item">
                                     <div class="form-group">
+                                        <label class="form-check-label" for="acceptTermsCheckbox">
                                         <input type="checkbox" name="terms" class="form-check-input acceptTermsCheckbox"
                                             id="acceptTermsCheckbox" required>
-                                        <label class="form-check-label" for="acceptTermsCheckbox">Kişisel verilerimin işlenmesine
+                                                Kişisel verilerimin işlenmesine
                                             <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal">"açık rıza metni"</a> kapsamında onay
                                             veriyorum.
                                         </label>
+                                        <br>
+                                        <br>
+                                        <p>Kişisel verileriniz PORT NATURE tarafından iletişim faaliyetlerinin yürütülmesi amacıyla işlenecektir. Kişisel verilerinizin işlenmesi ile ilgili ayrıntılı bilgiye <a href="/iletisim-politikasi">Aydınlatma Metninden</a> erişebilirsiniz.</p>
                                         <div class="modal fade" id="exampleModal" tabindex="-1"
                                             aria-labelledby="exampleModalLabel" aria-hidden="true">
                                             <div class="modal-dialog vh-100 d-flex align-items-center justify-content-center">
                                                 <div class="modal-content">
                                                     <div class="modal-header">
-                                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Terms & Cond</h1>
+                                                        <p class="modal-title fs-5" id="exampleModalLabel">Açık Rıza Metni</p>
                                                         <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                             aria-label="Close"></button>
                                                     </div>
                                                     <div class="modal-body d-flex flex-column justify-content-center text-center">
-                                                        <p class="text-uppercase" style="font-weight: bold">Açık Rıza Metni</p>
-                                                        <p class="text-start"> İletişim formu aracılığıyla işlenen kişisel verileriniz ile ilgili 6698 Sayılı Kişisel Verilerin Korunması m.10 gereği Aydınlatma Yükümlülüğünü yerine getirmek ve şeffaf olabilmek amacıyla hazırladığımız aydınlatma metnine <a href="/iletisim-politikasi">"buradan"</a> ulaşabilirsiniz
-                                                        </p>
+                                                        <p><strong></strong></p>
+
+                                                        <p>6698 sayılı Kişisel Verilerin Korunması Kanunu&#39;na g&ouml;re sizinle ilgili bazı kişisel verilerin, veri sorumlusu olan <strong>ARAS İNŞAAT TİCARET TURİZM ANONİM ŞİRKETİ (&lsquo;PORT NATURE&rsquo; olarak anılacaktır.) </strong>tarafından işlenebilmesi i&ccedil;in a&ccedil;ık rızanız gerekmektedir.</p>
+                            
+                                                        <p>&nbsp;</p>
+                            
+                                                        <p>Bu kapsamda İletişim faaliyetlerinin y&uuml;r&uuml;t&uuml;lmesi ve sorunuzun bize iletilmesi amacıyla <strong>Kimlik</strong> <em>(Ad, Soyad),</em> <strong>İletişim </strong><em>(e-Mail),</em><strong> İşlem G&uuml;venliği </strong><em>(İşlem Tarihi), </em><strong><em>Ziyaret&ccedil;i</em> İşlem<em> </em></strong><em>(Mesaj Konusu), </em><strong>Hukuki İşlem</strong><em> (Saat, Tarih) </em>kişisel verileriniz KVKK Md.5/1&rsquo;de &ouml;ng&ouml;r&uuml;len a&ccedil;ık rızanıza istinaden işlenecektir.</p>
+                            
+                                                        <p>&nbsp;</p>
+                            
+                                                        <p><strong>Yukarıda ama&ccedil;ları belirtilen kişisel verilerimin belirtilen ama&ccedil;lar &ccedil;er&ccedil;evesinde işlenmesini kabul ve beyan ediyorum.</strong></p>
+                            
         
         
                                                     </div>
