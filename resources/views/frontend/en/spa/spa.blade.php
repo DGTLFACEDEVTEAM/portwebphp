ce9d5dc35e979212986911f72b889e54e3471fef
Cenk Arslan <c.arslan3509@gmail.com>
2024-04-01 17:35:55 +0300

    salt room removed

--- resources/views/frontend/en/spa/spa.blade.php
+++ resources/views/frontend/en/spa/spa.blade.php
@@ -66,12 +66,12 @@
                 </figure>
                 <h5>Sauna</h5>
             </div>
-            <div class="swiper-slide">
+            {{-- <div class="swiper-slide">
                 <figure>
                     <img src="{{ asset('assets/frontend/images/spa/Saltroom.webp') }}" alt="SaltRoom"/>
                 </figure>
                 <h5>Salt Room</h5>
-            </div>
+            </div> --}}
             <div class="swiper-slide">
                 <figure>
                     <img src="{{ asset('assets/frontend/images/spa/Aromatherapy.webp') }}" alt="Aromatherapy"/>
